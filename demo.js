521e512e2a1b5d4a37eff26d4e9bf9125b98adbc
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-07 10:37:47 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -25,30 +25,29 @@ function likeCallback(e) {
       mimic Pillar 3 (Server Communication) to only update the screen if the
       sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
     });
 }
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, the elements we identify in STEP 1 need to be told to run that update
-// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
-// this code.
+STEP 3: In order for the call to the server and the update of the screen to
+work, the elements we identify in STEP 1 need to be told to run that update
+code when an "event" is fired. That's Pillar 2, event handling. Uncomment
+this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-// STEP 4:
-
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
+STEP 4:
+When all the STEPs' code changes have been complete, you'll be able to see a
+working demonstration of our reference example. Sure, it's maybe not as
+pretty as a professional site, but they're only different from our site in
+style, not substance.
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
